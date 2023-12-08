f023f590f6f86ad763f5e10e636c5c3cb06ca4e4
ShubhamKaudewar <shubham.kaudewar@wealthtech.in>
2023-12-09 00:09:35 +0530

    schedular has been added and tested

--- bot.py
+++ bot.py
@@ -1,28 +1,28 @@
 
+
 from os import getenv
-from discord.ext import commands
-import discord
 from dotenv import load_dotenv
 load_dotenv()
-import bot
-from datetime import datetime, time, timedelta
-import asyncio
-TOKEN = getenv("BOT_TOKEN")
-client = discord.Client(intents=discord.Intents.default())
+
+from discord import Client, Intents
+client = Client(intents=Intents.default())
+
+# setting trigger time
+from datetime import time, timezone
+utc = timezone.utc
+trigger_time = time(hour=5, minute=30, tzinfo=utc)
+
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
+    trigger_wotd.start()
+    trigger_potd.start()
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
 
-@client.event
-async def trigger(channel_id):
+from discord.ext import tasks
+@tasks.loop(time=trigger_time)
+async def trigger_potd():
     from wikimedia.image import fetch_image
     image_data = fetch_image()
     image_url = image_data.get("image_url")
@@ -31,7 +31,7 @@ async def trigger(channel_id):
     caption = fetch_caption(image_page_id)
     description = "**Wikimedia Commons: Picture of the day** \n\n## _*" + caption + "*_"
 
-    channel = client.get_channel(channel_id)  # replace id
+    channel = client.get_channel(int(getenv("POTD_CHANNEL")))
     from aiohttp import ClientSession
     async with ClientSession() as session:  # creates session
         async with session.get(image_url) as resp:  # gets image from url
@@ -42,68 +42,35 @@ async def trigger(channel_id):
                 from discord import File
                 await channel.send(description, file=File(file, "t.png"))
 
-@client.event
-async def trigger_wotd(channel_id):
+
+@tasks.loop(time=trigger_time)
+async def trigger_wotd():
     from wordnik.word import WordNik
+    channel = client.get_channel((int(getenv("WOTD_CHANNEL"))))
+    from datetime import date, datetime
+    cur_date = date.today()
+    date_iso = cur_date.isoformat()
+    word_text = WordNik().get_WOTD(date_iso)
 
-    channel = client.get_channel(channel_id)  # replace id
-    from aiohttp import ClientSession
-    async with ClientSession() as session:  # creates session
+    x_date = datetime.now()
+    today = x_date.strftime("%A,%d %B, %Y")
+    caption = f'WordNik API Present: Word of the day [{today}]\n{word_text}'
+    await channel.send(caption)
 
-        from datetime import date, datetime
-        cur_date = date.today()
-        date_iso = cur_date.isoformat()
-        word_text = WordNik().get_WOTD(date_iso)
+client.run(getenv("BOT_TOKEN"))
 
-        x_date = datetime.now()
-        today = x_date.strftime("%A,%d %B, %Y")
-        caption = f'WordNik API Present: Word of the day [{today}]\n{word_text}'
-        await channel.send(caption)
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content == '$potd':
-        channel_id = message.channel.id
-        await trigger(channel_id)
-    elif message.content == '$wotd':
-        channel_id = message.channel.id
-        await trigger_wotd(channel_id)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
-
-
-client.run(TOKEN)
-
-
-# FIXME: Below function for schedular
-# bot = commands.Bot(command_prefix="$")
-# WHEN = time(18, 0, 0)
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 #
-# async def called_once_a_day():  # Fired every day
-#     await bot.wait_until_ready()  # Make sure your guild cache is ready so the channel can be found via get_channel
-#     await trigger()
-#
-#
-# async def background_task():
-#     now = datetime.utcnow()
-#     if now.time() > WHEN:  # Make sure loop doesn't start after {WHEN} as then it will send immediately the first time as negative seconds will make the sleep yield instantly
-#         tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
-#         seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
-#         await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start
-#     while True:
-#         now = datetime.utcnow() # You can do now() or a specific timezone if that matters, but I'll leave it with utcnow
-#         target_time = datetime.combine(now.date(), WHEN)  # 6:00 PM today (In UTC)
-#         seconds_until_target = (target_time - now).total_seconds()
-#         await asyncio.sleep(seconds_until_target)  # Sleep until we hit the target time
-#         await called_once_a_day()  # Call the helper function that sends the message
-#         tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
-#         seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
-#         await asyncio.sleep(seconds)
-#
-# if __name__ == "__main__":
-#     bot.loop.create_task(background_task())
-#     bot.run(TOKEN)
+#     if message.content == '$potd':
+#         channel_id = message.channel.id
+#         await trigger(channel_id)
+#     elif message.content == '$wotd':
+#         channel_id = message.channel.id
+#         await trigger_wotd(channel_id)
+#     elif message.content == 'raise-exception':
+#         raise discord.DiscordException
+
 
